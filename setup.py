12d5b7fc6cff7587647f67ea2747a6f014cb5d18
n0n0a <n0n0a6262@gmail.com>
2021-06-24 09:18:15 +0900

    update: setup.py

--- setup.py
+++ setup.py
@@ -8,14 +8,14 @@ INSTALL_REQUIRES = [
 ]
 
 setup(
-    name='blackjack-rl',
+    name='blackjack_rl',
     version="0.0.1",
-    description='reinforcement learning for blackjack game',
+    description='Reinforcement Learning for Blackjack Game',
     author='n0n0a',
     url="https://github.com/n0n0a/blackjack-rl",
     license='MIT',
     install_requires=INSTALL_REQUIRES,
-    packages=find_packages(),
+    packages=find_packages(exclude=["test"]),
     classifiers=[
         "Programming Language :: Python :: 3.9",
         "License :: OSI Approved :: MIT License"

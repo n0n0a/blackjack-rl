939f786ec3fbdddd808bea483f4c7469a3348e27
n0n0a <n0n0a6262@gmail.com>
2021-06-24 15:01:29 +0900

    feat: seed test

--- test/test_hand.py
+++ test/test_hand.py
@@ -13,8 +13,9 @@ def test_constructor():
 
 def test_draw():
     np_random, seed = seeding.np_random(5)
+
     # 10 False Start
-    hand = Hand(sum=10, have_eleven_ace=False, np_random=np_random)
+    hand = Hand(sum=10, have_eleven_ace=False)
     assert hand.sum == 10
     assert not hand.have_eleven_ace
     hand.draw(card=1)
@@ -26,8 +27,9 @@ def test_draw():
     hand.draw(card=1)
     assert hand.sum == 13
     assert not hand.have_eleven_ace
+
     # 11 True Start
-    hand = Hand(sum=11, have_eleven_ace=True, np_random=np_random)
+    hand = Hand(sum=11, have_eleven_ace=True)
     assert hand.sum == 11
     assert hand.have_eleven_ace
     hand.draw(card=1)
@@ -42,8 +44,9 @@ def test_draw():
     hand.draw(card=10)
     assert hand.sum == 23
     assert not hand.have_eleven_ace
+
     # 11 False Start
-    hand = Hand(sum=11, have_eleven_ace=False, np_random=np_random)
+    hand = Hand(sum=11, have_eleven_ace=False)
     assert hand.sum == 11
     assert not hand.have_eleven_ace
     hand.draw(card=1)
@@ -51,4 +54,16 @@ def test_draw():
     assert not hand.have_eleven_ace
     hand.draw(card=10)
     assert hand.sum == 22
+    assert not hand.have_eleven_ace
+
+    # np_random test
+    hand = Hand(np_random=np_random)
+    hand.draw()
+    assert hand.sum == 10
+    assert not hand.have_eleven_ace
+    hand.draw()
+    assert hand.sum == 18
+    assert not hand.have_eleven_ace
+    hand.draw()
+    assert hand.sum == 25
     assert not hand.have_eleven_ace

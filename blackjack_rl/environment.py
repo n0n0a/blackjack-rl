7b1cd3fbe9c1043196e63fcc8a05bb7ba65e247f
n0n0a <n0n0a6262@gmail.com>
2021-06-25 11:29:20 +0900

    feat: run_one_game

--- blackjack_rl/environment.py
+++ blackjack_rl/environment.py
@@ -85,15 +85,28 @@ class BlackjackEnv(gym.Env):
     def make_samples(self, episode: int) -> List[Tuple[Tuple[int, int, bool], bool, int, Tuple[int, int, bool]]]:
         samples = []
         for _ in range(episode):
-            self.player.sum = self.np_random.choice(range(12, 21))
-            self.player.have_eleven_ace = self.np_random.choice([True, False])
-            self.dealer.sum = self.np_random.choice(range(2, 11))
-            self.dealer.have_eleven_ace = self.dealer.sum == 11
-            observation = self._get_obs()
-            done = False
-            reward = 0
-            while not done:
-                action = self.np_random.choice([True, False])
-                next_observation, reward, done, info = self.step(action)
-                samples.append((observation, action, reward, next_observation))
+            player = Hand(sum=self.np_random.choice(range(12, 21)),
+                          have_eleven_ace=self.np_random.choice([True, False]),
+                          np_random=self.np_random)
+            dealer_draw = self.np_random.choice(range(2, 11))
+            dealer = Hand(sum=dealer_draw,
+                          have_eleven_ace=dealer_draw==11,
+                          np_random=self.np_random)
+            samples.extend(self.run_one_game(init_hand=(player, dealer)))
         return samples
+
+    def run_one_game(self, init_hand: Tuple[Hand, Hand] = None) -> List[Tuple[Tuple[int, int, bool], bool, int, Tuple[int, int, bool]]]:
+        if init_hand is None:
+            self.reset()
+        else:
+            self.player = Hand[0]
+            self.dealer = Hand[1]
+        observation = self._get_obs()
+        done = False
+        reward = 0
+        trajectory = []
+        while not done:
+            action = self.np_random.choice([True, False])
+            next_observation, reward, done, info = self.step(action)
+            trajectory.append((observation, action, reward, next_observation))
+        return trajectory

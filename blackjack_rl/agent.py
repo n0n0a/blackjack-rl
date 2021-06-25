a314388cbe6fe3b2fc9aa351421c8aebef009d3b
n0n0a <n0n0a6262@gmail.com>
2021-06-25 12:58:13 +0900

    fix: base agent

--- blackjack_rl/agent.py
+++ blackjack_rl/agent.py
@@ -2,12 +2,12 @@ from abc import ABCMeta, abstractmethod
 from typing import Tuple, List, Union
 
 
-class Agent(ABCMeta):
+class Agent:
     @abstractmethod
-    def take_action(cls) -> bool:
+    def take_action(self, state: Tuple[int, int, bool]) -> bool:
         raise NotImplementedError
 
     @abstractmethod
-    def train(cls, data: Union[List[Tuple[Tuple[int, int, bool], bool, int, Tuple[int, int, bool]]],
+    def train(self, data: Union[List[Tuple[Tuple[int, int, bool], bool, int, Tuple[int, int, bool]]],
                                Tuple[Tuple[int, int, bool], bool, int, Tuple[int, int, bool]]]):
         raise NotImplementedError

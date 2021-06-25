d4d32c1d0d5cc090b2723e3bd574354bd0f0799d
n0n0a <n0n0a6262@gmail.com>
2021-06-26 02:36:59 +0900

    change: Agent based ABC

--- blackjack_rl/agent.py
+++ blackjack_rl/agent.py
@@ -1,13 +1,13 @@
-from abc import ABCMeta, abstractmethod
+from abc import ABC, abstractmethod
 from typing import Tuple, List
 from blackjack_rl.typedef import State, Action, Trans
 
 
-class Agent:
+class Agent(ABC):
     @abstractmethod
-    def take_action(self, state: State) -> Action:
+    def take_action(cls, state: State) -> Action:
         raise NotImplementedError
 
     @abstractmethod
-    def train(self, train_data: List[Trans]):
+    def train(cls, train_data: List[Trans]):
         raise NotImplementedError

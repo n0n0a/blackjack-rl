e4ac7ad12819b46245adcc466245cdebb64a5606
n0n0a <n0n0a6262@gmail.com>
2021-06-25 12:02:05 +0900

    add: typing for base agent

--- blackjack_rl/agent.py
+++ blackjack_rl/agent.py
@@ -1,11 +1,13 @@
 from abc import ABCMeta, abstractmethod
+from typing import Tuple, List, Union
 
 
 class Agent(ABCMeta):
     @abstractmethod
-    def take_action(cls):
+    def take_action(cls) -> bool:
         raise NotImplementedError
 
     @abstractmethod
-    def train(cls, data):
+    def train(cls, data: Union[List[Tuple[Tuple[int, int, bool], bool, int, Tuple[int, int, bool]]],
+                               Tuple[Tuple[int, int, bool], bool, int, Tuple[int, int, bool]]]):
         raise NotImplementedError

5cb2a47c8c03d6ffcb1e00118eca15b960d22f1a
n0n0a <n0n0a6262@gmail.com>
2021-06-26 19:26:14 +0900

    feat: multiprocess train

--- blackjack_rl/lspi.py
+++ blackjack_rl/lspi.py
@@ -4,6 +4,7 @@ import numpy as np
 from scipy.sparse import csc_matrix
 from scipy.sparse.linalg import inv
 from blackjack_rl.typedef import State, Action, Trans
+from concurrent import futures
 
 # index = 0
 dealer_offset = 2
@@ -13,7 +14,23 @@ all_space = (10, 9, 2, 2)
 # all space size
 all_size = np.prod(all_space)
 # regularize factor
-episilon = 0.000001
+episilon = 0.0000001
+
+
+# out of class due to multiprocessing
+def calculate_sum(data, weights):
+    st, at, rtt, stt = data
+    phi_t, phi_tt = csc_matrix((all_size, 1), dtype=float), csc_matrix((all_size, 1), dtype=float)
+    phi_t[LSPIAgent._translate_weight_idx(st, at), 0] = 1
+    if LSPIAgent._isvalid(state=stt):
+        action = True
+        if st[1] >= 12:
+            action = weights[LSPIAgent._translate_weight_idx(state=LSPIAgent._reindex_state(stt), action=True)] \
+                     > weights[LSPIAgent._translate_weight_idx(state=LSPIAgent._reindex_state(stt), action=False)]
+        phi_tt[LSPIAgent._translate_weight_idx(LSPIAgent._reindex_state(stt), action), 0] = 1
+    td_phi = phi_t - phi_tt
+    td_phi = csc_matrix(td_phi.toarray().T)
+    return phi_t * td_phi, phi_t * rtt
 
 
 class LSPIAgent(Agent):
@@ -49,12 +66,27 @@ class LSPIAgent(Agent):
             # only use appropriate data
             train_data = [(self._reindex_state(d[0]), d[1], d[2], d[3]) for d in train_data if self._isvalid(d[0])]
             if not train_data: return
-            A = self._calculate_A(train_data)
-            b = self._calculate_b(train_data)
+            # A = self._calculate_A(train_data)
+            # b = self._calculate_b(train_data)
+            A, b = self._calculate_ab(train_data)
             # after regularization, inverse matrix
             # new_weight = inv(A) * b
             new_weight = inv(A + csc_matrix(episilon * np.eye(all_size, dtype=float), dtype=float)) * b
             self.weight = new_weight.toarray().ravel()
+
+    # calculate A for updating weight
+    def _calculate_ab(self, train_data: List[Trans]) -> Tuple[csc_matrix, csc_matrix]:
+
+        A = csc_matrix((all_size, all_size), dtype=float)
+        b = csc_matrix((all_size, 1), dtype=float)
+        results = None
+        weights = [self.weight] * len(train_data)
+        with futures.ProcessPoolExecutor() as executor:
+            results = executor.map(calculate_sum, train_data, weights, chunksize=10)
+        for res in results:
+            A += res[0]
+            b += res[1]
+        return A, b
 
     # calculate A for updating weight
     def _calculate_A(self, train_data: List[Trans]) -> csc_matrix:
@@ -81,17 +113,20 @@ class LSPIAgent(Agent):
         return b
 
     # included in state space
-    def _isvalid(self, state: State) -> bool:
+    @staticmethod
+    def _isvalid(state: State) -> bool:
         return (2 <= state[0] <= 11) and (12 <= state[1] <= 20)
 
     # reindex dealer and player state
-    def _reindex_state(self, state: State) -> State:
-        assert self._isvalid(state)
+    @staticmethod
+    def _reindex_state(state: State) -> State:
+        assert LSPIAgent._isvalid(state)
         dealer, player, have = state
         return dealer - dealer_offset, player - player_offset, have
 
     # function phi(s,a): (s,a) -> R^(all_size)
-    def _translate_weight_idx(self, state: State, action: Action) -> int:
+    @staticmethod
+    def _translate_weight_idx(state: State, action: Action) -> int:
         offset = 1
         idx = 0
         all = state + (action,)

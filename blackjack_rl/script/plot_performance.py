e4aae406922fa1fda7942cbe4b9b410721b97517
Eater <Eater@DESKTOP-VCK2AER>
2021-07-05 00:43:39 +0900

    xが100決め打ちだったのでepoch数が変わってもいいようにlen(data)にした

--- blackjack_rl/script/plot_performance.py
+++ blackjack_rl/script/plot_performance.py
@@ -14,11 +14,11 @@ def plot_performance():
     paths = [lspi_path, monte_path, qlearning_path]
     names = ["lspi", "monte", "qlearning"]
     fig = plt.figure()
-    x = list(range(100))
     for idx, path in enumerate(paths):
         if os.path.exists(path):
             with open(path, "rb") as f:
                 data = pickle.load(f)
+                x = list(range(len(data)))
                 print(data)
                 ax = fig.add_subplot(1, 1, 1)
                 ax.plot(x, data, label=names[idx])
